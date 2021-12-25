f74a4edc02c0680e8afbed4eaa6088d422d833e2
CRISTIIV <vicente.cristi@alumnos.ucn.cl>
2021-12-24 21:57:24 -0300

    Create Encomienda.java

---
+++ src/dominio/Encomienda.java
@@ -0,0 +1,41 @@
+package dominio;
+
+public class Encomienda extends Entrega{
+	private int largo;
+	private int ancho;
+	private int deep;
+	public Encomienda(int peso, String codigo, String rutRemitente, String rutDestinatario, int largo, int ancho,
+			int deep) {
+		super(peso, codigo, rutRemitente, rutDestinatario);
+		this.largo = largo;
+		this.ancho = ancho;
+		this.deep = deep;
+	}
+	public int getLargo() {
+		return largo;
+	}
+	public void setLargo(int largo) {
+		this.largo = largo;
+	}
+	public int getAncho() {
+		return ancho;
+	}
+	public void setAncho(int ancho) {
+		this.ancho = ancho;
+	}
+	public int getDeep() {
+		return deep;
+	}
+	public void setDeep(int deep) {
+		this.deep = deep;
+	}
+	@Override
+	public double calcularPrecio() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	public String toStringEncomienda() {
+		return "Tipo de entrega: Encomienda, Ancho de la encomienda: "+ancho+"m, Largo de la encomienda: "+largo+"m, Profundidad de la encomienda: "+deep+"m, "+super.toStringEntrega()+"\n";
+	}
+}
