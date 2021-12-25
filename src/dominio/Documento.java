c2b43997fc59c9407a914724c56f8d54bff75edb
CRISTIIV <vicente.cristi@alumnos.ucn.cl>
2021-12-24 21:57:22 -0300

    Create Documento.java

---
+++ src/dominio/Documento.java
@@ -0,0 +1,28 @@
+package dominio;
+
+public class Documento extends Entrega{
+	private int grosor;
+	
+	public Documento(int peso, String codigo, String rutRemitente, String rutDestinatario, int grosor) {
+		super(peso, codigo, rutRemitente, rutDestinatario);
+		this.grosor = grosor;
+	}
+
+	public int getGrosor() {
+		return grosor;
+	}
+
+	public void setGrosor(int grosor) {
+		this.grosor = grosor;
+	}
+
+	@Override
+	public double calcularPrecio() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	public String toStringDocumento() {
+		return "Tipo de entrega: Documento, Grosor del documento: "+grosor+"cm, "+super.toStringEntrega()+"\n";
+	}
+}
