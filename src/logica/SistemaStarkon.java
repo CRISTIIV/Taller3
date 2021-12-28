827be3e4da180f946c0586db33478a3f2fcaa30d
CRISTIIV <vicente.cristi@alumnos.ucn.cl>
2021-12-28 16:47:15 -0300

    Update SistemaStarkon.java

--- src/logica/SistemaStarkon.java
+++ src/logica/SistemaStarkon.java
@@ -3,11 +3,11 @@ package logica;
 public interface SistemaStarkon {
 	boolean agregarCliente(String rut, String nombre, String apellido, int saldo, String ciudad);
 	boolean agregarLocalizacion(String nombre);
-	boolean agregarDocumento(int peso, String codigo, String rutRemitente, String rutDestinatario, int grosor);
-	boolean agregarEncomienda(int peso, String codigo, String rutRemitente, String rutDestinatario, int largo, int ancho, int profundidad);
-	boolean agregarValija(int peso, String codigo, String rutRemitente, String rutDestinatario, String material);
-	void asociarEntregaRemitenteDestinatario(String codigo, String rutRemitente, String rutDestinatario);
-	void asociarLocalidadCliente(String rutCliente, String ciudad);
+	void agregarDocumento(int peso, String codigo, String rutRemitente, String rutDestinatario, int grosor);
+	void agregarEncomienda(int peso, String codigo, String rutRemitente, String rutDestinatario, int largo, int ancho, int profundidad);
+	void agregarValija(int peso, String codigo, String rutRemitente, String rutDestinatario, String material);
+	//void asociarEntregaRemitenteDestinatario(String codigo, String rutRemitente, String rutDestinatario);
+	//void asociarLocalidadCliente(String rutCliente, String ciudad); //CREO QUE ACÁ COMO NO HAY CONEXIÓN NO ES NECESARIA LA ASOCIACIÓN ENTRE CLASES.
 	void realizarEntrega(String rutRemitente, String tipoPaquete, String rutDestinatario);
 	void recargarSaldo(String rutCliente, int saldo);
 	String iniciarSesion(String rut);
