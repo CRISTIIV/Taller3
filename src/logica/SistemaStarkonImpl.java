7b3589c0072be079619e0019694f93fb966d2779
CRISTIIV <vicente.cristi@alumnos.ucn.cl>
2021-12-28 16:47:17 -0300

    Update SistemaStarkonImpl.java

--- src/logica/SistemaStarkonImpl.java
+++ src/logica/SistemaStarkonImpl.java
@@ -1,39 +1,97 @@
 package logica;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+import dominio.*;
 
 public class SistemaStarkonImpl implements SistemaStarkon{
-
+	private ListaCircularDobleEntrega entregas;
+	private Lista<Documento> documentos;
+	private Lista<Encomienda> encomiendas;
+	private Lista<Valija> valijas;
+	private LinkedList<Cliente> clientes;
+	private ArrayList<Localizacion> localizaciones;
+	
+	public SistemaStarkonImpl() {
+		entregas = new ListaCircularDobleEntrega();
+		documentos = new ListaCircularDoble<Documento>();
+		encomiendas = new ListaCircularDoble<Encomienda>();
+		valijas = new ListaCircularDoble<Valija>();
+		clientes = new LinkedList<Cliente>();
+		localizaciones = new ArrayList<Localizacion>();
+	}
+	
+	
 	@Override
 	public boolean agregarCliente(String rut, String nombre, String apellido, int saldo, String ciudad) {
-		// TODO Auto-generated method stub
-		return false;
+		for (Cliente cliente:clientes) {
+			if(cliente.getRut().equals(rut))return false;
+		}
+		Cliente c = new Cliente(rut,nombre,apellido,saldo,ciudad);
+		return clientes.add(c);
 	}
 
 	@Override
 	public boolean agregarLocalizacion(String nombre) {
-		// TODO Auto-generated method stub
-		return false;
+		for (Localizacion localizacion:localizaciones) {
+			if(localizacion.getNombre().equals(nombre))return false;
+		}
+		Localizacion l = new Localizacion(nombre);
+		return localizaciones.add(l);
 	}
 
 	@Override
-	public boolean agregarDocumento(int peso, String codigo, String rutRemitente, String rutDestinatario, int grosor) {
-		// TODO Auto-generated method stub
-		return false;
+	public void agregarDocumento(int peso, String codigo, String rutRemitente, String rutDestinatario, int grosor) {
+		Documento d = new Documento(peso,codigo,rutRemitente,rutDestinatario,grosor);
+		for(Cliente cliente:clientes) {
+			if(cliente.getRut().equals(rutRemitente)) {
+				cliente.getEntregasE().add(d);
+				break;
+			}
+			if(cliente.getRut().equals(rutDestinatario)) {
+				cliente.getEntregasR().add(d);
+				break;
+			}
+		}
+		entregas.add(d);
+		documentos.add(d);
 	}
 
 	@Override
-	public boolean agregarEncomienda(int peso, String codigo, String rutRemitente, String rutDestinatario, int largo,
-			int ancho, int profundidad) {
-		// TODO Auto-generated method stub
-		return false;
+	public void agregarEncomienda(int peso, String codigo, String rutRemitente, String rutDestinatario, int largo, int ancho, int profundidad) {
+		Encomienda e = new Encomienda(peso,codigo,rutRemitente,rutDestinatario,largo,ancho,profundidad);
+		for(Cliente cliente:clientes) {
+			if(cliente.getRut().equals(rutRemitente)) {
+				cliente.getEntregasE().add(e);
+				break;
+			}
+			if(cliente.getRut().equals(rutDestinatario)) {
+				cliente.getEntregasR().add(e);
+				break;
+			}
+		}
+		entregas.add(e);
+		encomiendas.add(e);
 	}
 
 	@Override
-	public boolean agregarValija(int peso, String codigo, String rutRemitente, String rutDestinatario,
-			String material) {
-		// TODO Auto-generated method stub
-		return false;
+	public void agregarValija(int peso, String codigo, String rutRemitente, String rutDestinatario, String material) {
+		Valija v = new Valija(peso,codigo,rutRemitente,rutDestinatario,material);
+		for(Cliente cliente:clientes) {
+			if(cliente.getRut().equals(rutRemitente)) {
+				cliente.getEntregasE().add(v);
+				break;
+			}
+			if(cliente.getRut().equals(rutDestinatario)) {
+				cliente.getEntregasR().add(v);
+				break;
+			}
+		}
+		entregas.add(v);
+		valijas.add(v);
 	}
-
+	
+	/*
 	@Override
 	public void asociarEntregaRemitenteDestinatario(String codigo, String rutRemitente, String rutDestinatario) {
 		// TODO Auto-generated method stub
@@ -42,9 +100,17 @@ public class SistemaStarkonImpl implements SistemaStarkon{
 
 	@Override
 	public void asociarLocalidadCliente(String rutCliente, String ciudad) {
-		// TODO Auto-generated method stub
+		Cliente cliente = null;
+		for(Cliente client:clientes) {
+			if(client.getRut().equals(rutCliente))cliente = (Cliente)client;
+		}
+		for(Localizacion localizacion:localizaciones) {
+			if(localizacion.getNombre().equals(ciudad)) {
+				cliente.g
+			}
+		}
 		
-	}
+	}*/
 
 	@Override
 	public void realizarEntrega(String rutRemitente, String tipoPaquete, String rutDestinatario) {
@@ -54,20 +120,44 @@ public class SistemaStarkonImpl implements SistemaStarkon{
 
 	@Override
 	public void recargarSaldo(String rutCliente, int saldo) {
-		// TODO Auto-generated method stub
+		for(Cliente cliente:clientes) {
+			if(cliente.getRut().equals(rutCliente)) {
+				//Cliente c = (Cliente)cliente;
+				cliente.aumentarSaldo(saldo);//c
+				return;
+			}
+		}
+		throw new NullPointerException("No existe el cliente seleccionado");
 		
 	}
 
 	@Override
 	public String iniciarSesion(String rut) {
-		// TODO Auto-generated method stub
-		return null;
+		Cliente client = null;
+		for(Cliente cliente:clientes) {
+			if(cliente.getRut().equals(rut)) {
+				client = cliente;
+			}
+		}
+		if(client==null) {
+			return "Registrar";
+		}
+		if(!rut.equals("Admin")) {
+			return "Cliente-"+client.getNombre()+" "+client.getApellido();
+		}else {
+			return "Admin";
+		}
 	}
 
 	@Override
 	public String obtenerEntregasRecibosCliente(String rutCliente) {
-		// TODO Auto-generated method stub
-		return null;
+		Cliente client = null;
+		for(Cliente cliente:clientes) {
+			if(cliente.getRut().equals(rutCliente)) {
+				client = cliente;
+			}
+		}
+		return client.enviadasRecibidas();
 	}
 
 	@Override
