e112daf6e54f2b7491369d781e5c1b2920d69b36
CRISTIIV <vicente.cristi@alumnos.ucn.cl>
2021-12-29 16:58:00 -0300

    Update SistemaStarkon.java

--- src/logica/SistemaStarkon.java
+++ src/logica/SistemaStarkon.java
@@ -8,6 +8,9 @@ public interface SistemaStarkon {
 	void agregarValija(int peso, String codigo, String rutRemitente, String rutDestinatario, String tipo, String material);
 	String obtenerClientes();
 	String obtenerEntregas();
+	void realizarEntregaDocumento(String rutRemitente,String codigo, String rutDestinatario, int peso, int grosor);
+	void realizarEntregaEncomienda(String rutRemitente,String codigo, String rutDestinatario, int peso, int largo, int ancho, int profundidad);
+	void realizarEntregaValija(String rutRemitente,String codigo, String rutDestinatario, int peso, String material);
 	void recargarSaldo(String rutCliente, int saldo);
 	String iniciarSesion(String rut);
 	String obtenerEntregasRecibosCliente(String rutCliente);
