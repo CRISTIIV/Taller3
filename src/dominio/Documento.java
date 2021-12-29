54b8751e7f700c481187edd438d624ba85cc6697
CRISTIIV <vicente.cristi@alumnos.ucn.cl>
2021-12-29 16:58:10 -0300

    Update Documento.java

--- src/dominio/Documento.java
+++ src/dominio/Documento.java
@@ -3,8 +3,8 @@ package dominio;
 public class Documento extends Entrega{
 	private int grosor;
 	
-	public Documento(int peso, String codigo, String rutRemitente, String rutDestinatario, int grosor) {
-		super(peso, codigo, rutRemitente, rutDestinatario);
+	public Documento(int peso, String codigo, String rutRemitente, String rutDestinatario, String tipo, int grosor) {
+		super(peso, codigo, rutRemitente, rutDestinatario, tipo);
 		this.grosor = grosor;
 	}
 
@@ -18,10 +18,10 @@ public class Documento extends Entrega{
 
 	@Override
 	public double calcularPrecio() {
-		return (this.getPeso()*grosor*100);
+		return ((this.getPeso()*0.001)*grosor*100);
 	}
 	
 	public String toStringDocumento() {
-		return "Tipo de entrega: Documento, Grosor del documento: "+grosor+"cm, "+super.toStringEntrega()+"\n";
+		return "Tipo de entrega: Documento, Grosor del documento: "+grosor+"cm, Valor de la entrega: $"+calcularPrecio()+", "+super.toStringEntrega()+"\n";
 	}
 }
